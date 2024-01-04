09824a6238d7a651fce16868f5b4eabd9ff40f7a
M Fadli Ridhani <mfadliridhani@192.168.0.7>
2024-01-04 21:19:44 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// alert(1 + 1);
+// console.log('Hello Word');
+
+let math = Math.random();
+console.log(math);
+
+if (math >= 0.5) {
+    console.log('Math score equal to or more than 0.5');
+}
+
+if (math <= 0.5) {
+    console.log('Math score equal to or less than 0.5');
+}
